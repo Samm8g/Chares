835963e6575db8046c58bada29f7037e3a66d7e7
Samm <samm8gg@gmail.com>
2025-09-10 22:02:49 +0300

    Haptic Feedback improvement

--- app/src/main/java/com/samm8g/chares/data/preferences/HapticManager.kt
+++ app/src/main/java/com/samm8g/chares/data/preferences/HapticManager.kt
@@ -12,6 +12,7 @@ class HapticManager(context: Context) {
 
     companion object {
         val HAPTIC_FEEDBACK_KEY = booleanPreferencesKey("haptic_feedback_option")
+        val ANIMATIONS_ENABLED_KEY = booleanPreferencesKey("animations_enabled_option")
     }
 
     val hapticFeedback: Flow<Boolean> = dataStore.data.map {
@@ -19,10 +20,22 @@ class HapticManager(context: Context) {
         preferences[HAPTIC_FEEDBACK_KEY] ?: true // Default to enabled
     }
 
+    val animationsEnabled: Flow<Boolean> = dataStore.data.map {
+        preferences ->
+        preferences[ANIMATIONS_ENABLED_KEY] ?: true // Default to enabled
+    }
+
     suspend fun setHapticFeedback(isHapticFeedbackEnabled: Boolean) {
         dataStore.edit {
             preferences ->
             preferences[HAPTIC_FEEDBACK_KEY] = isHapticFeedbackEnabled
+        }
+    }
+
+    suspend fun setAnimationsEnabled(areAnimationsEnabled: Boolean) {
+        dataStore.edit {
+            preferences ->
+            preferences[ANIMATIONS_ENABLED_KEY] = areAnimationsEnabled
         }
     }
 }
