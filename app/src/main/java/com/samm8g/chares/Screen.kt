0db71ecd79e2af38def87c590593bb98ff05c986
Samm <samm8gg@gmail.com>
2026-06-10 22:29:52 +0300

    Leftovers

--- app/src/main/java/com/samm8g/chares/Screen.kt
+++ app/src/main/java/com/samm8g/chares/Screen.kt
@@ -2,7 +2,6 @@ package com.samm8g.chares
 
 sealed class Screen(val route: String) {
     object Home : Screen("home")
-    object AddChore : Screen("add_chore")
     object AllHistory : Screen("all_history")
     object Settings : Screen("settings")
     object About : Screen("about")
